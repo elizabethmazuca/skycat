f63124097decc3d85d2c032acb990ec4e7241fe6
elizabethmazuca <elizabethmazuca@csu.fullerton.edu>
2023-06-23 01:28:47 -0700

    still wont move on java lmao

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 alert('im aliveeee');
+
+let moon = document.getElementById('moon');
+
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+    moon.style.right = value * 0.50 + 'px'; 
+    moon.style.top = value * 2.3 + 'px';
+})
