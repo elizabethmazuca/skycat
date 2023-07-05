b0e6eef65b55447a54b8cf1e881986cd35e42683
elizabethmazuca <elizabethmazuca@csu.fullerton.edu>
2023-07-04 23:50:43 -0700

    FFFFF

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
     moon.style.right = value * 0.50 + 'px'; 
     moon.style.top = value * 2.3 + 'px';
+
+    /* FFFFFFF */
 })
